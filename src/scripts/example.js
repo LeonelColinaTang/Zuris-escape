b8111ff055519bcabaad820a3678cf4c3e74ea6a
leonelcolina <tang.leonel@gmail.com>
2022-07-23 19:16:34 -0400

    remove word when completed

--- src/scripts/example.js
+++ src/scripts/example.js
@@ -14,17 +14,47 @@ const randomWords = require('random-words');
 
 // export default Example;
 
-function something(){
-    // let url = 'https://api.dictionaryapi.dev/api/v2/entries/en/give';
-    let word = randomWords({ exactly: 1, maxLength: 4 }); //this generates random words of length up to 4
+
+
+
+//This function 
+function generateWord(){
+    let word = randomWords({ exactly: 1, maxLength: 4 })[0]; //this generates a random word of .length===4
     let container= document.getElementById('word-container');
-    console.log(container);
-    word[0].split("").forEach(letter => {
+
+    word.split("").forEach(letter => {
         let square = document.createElement("div");
         square.innerText = letter;
         square.classList.add('letter');
         container.appendChild(square);
     });
+
+    compareWord(word[0]);
 }
 
-something();
+generateWord();
+
+
+function compareWord(word){
+    
+    let i=0;
+    window.addEventListener('keydown', (event)=>{
+        if(event.key === word[i]){
+            alert(`Nice, "${event.key}" is the right letter`);
+            i++;
+        }else {
+            alert(`"${event.key}" is not the correct letter :( `);
+        }
+        if (word.length === i) {
+            //put some animation here maybe a promise
+            let letters = document.getElementsByClassName('letter');
+            Array.from(letters).forEach(ele=> ele.remove());
+            alert("word completed");
+        }
+    });
+
+
+
+}
+
+
