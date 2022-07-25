6cc6f7ad9f028d43cfe080bf5397099851c570c1
leonelcolina <tang.leonel@gmail.com>
2022-07-25 10:52:44 -0400

    create parallax effect

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ function generateWord() {
     compareWord(word);
 }
 
-// setInterval((generateWord),5000);
+setInterval((generateWord),5000);
 
 function compareWord(word) {
     let i = 0;
@@ -60,8 +60,6 @@ const ctx = canvas.getContext('2d');
 const CANVAS_WIDTH = canvas.width = 800;
 const CANVAS_HEIGHT = canvas.height = 715;
 let gameSpeed = 5;
-let x = 0;
-let x2 = 2400;
 
 const backgroundLayer1 = new Image();
 backgroundLayer1.src = 'src/layer-1.png';
@@ -75,20 +73,51 @@ const backgroundLayer5 = new Image();
 backgroundLayer5.src = 'src/layer-5.png';
 
 
+class Background{
+    constructor(image, speedModifier){
+        this.x = 0;
+        this.y = 0;
+        this.width = 2400;
+        this.height = 700;
+        this.x2 = this.width;
+        this.image = image;
+        this.speedModifier = speedModifier;
+        this.speed = gameSpeed * this.speedModifier;
+    }
+
+    update(){
+        this.speed = gameSpeed * this.speedModifier;
+        if (this.x <= -this.width){
+            this.x = this.width + this.x2 - this.speed;
+        }
+        if ( this.x2 <= -this.width){
+            this.x2 = this.width + this.x - this.speed;
+        }
+        this.x = Math.floor(this.x - this.speed);
+        this.x2 = Math.floor(this.x2 - this.speed);
+    }
+
+    draw(){
+        ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+        ctx.drawImage(this.image, this.x2, this.y, this.width, this.height);
+    }
+}
+
+
+
+const background1 = new Background(backgroundLayer1, 0.2);
+const background2 = new Background(backgroundLayer2, 0.4);
+const background3 = new Background(backgroundLayer3, 0.6);
+const background4 = new Background(backgroundLayer4, 0.8);
+const background5 = new Background(backgroundLayer5, 1);
+const gameBackgrounds = [background1, background2, background3, background4, background5];
+
 function animate() {
     ctx.clearRect(0,0, CANVAS_WIDTH, CANVAS_HEIGHT);
-
-
-    ctx.drawImage(backgroundLayer4, x, 0);
-    ctx.drawImage(backgroundLayer4, x2, 0);
-
-
-    if(x < -2400) x = 2400 + x2 - gameSpeed;
-    else x -=gameSpeed;
-
-    if (x2 < -2400) x2 = 2400 + x - gameSpeed;
-    else x2 -= gameSpeed;
-
+    gameBackgrounds.forEach(function(ele){
+        ele.update();
+        ele.draw();
+    })
 
 
     requestAnimationFrame(animate);
